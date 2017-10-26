0479608301931d03fdadaa786f7cfed49b94ef54
pashute <pashute@gmail.com>
2017-10-26 18:02:56 +0300

    First time iqfeeder feature

---
+++ src/IQFeeder/IQFeedImporter.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 16:20:22 2017
+
+@author: pashute 
+"""
+'''
+ see  either this: https://pypi.python.org/pypi/iqfeed/0.4.3
+ or BETTER:  https://github.com/luketighe/IQFeed 
+'''
+from iqFeed import historicData
+
+class IQFeedImporter():
+    def import_feed_symbol(self, dateStart, dateEnd, symbol):
+        iq = historicData(dateStart, dateEnd, 60)
+        symbolOneData = iq.download_symbol(symbolOne)
+        # todo: store in .mat file and in database
+    
+    def importFeedAll(self, dateStart, dateEnd):
+        #todo: read from symbols table
+        symbols = ['CBOT', 'CFE', "SPY", "AAPL", "GOOG", "AMZN"] 
+        for sym in symbols: 
+            data = self.import_feed_symbol(dateStart, dateEnd, sym)
+            data = "".join(data.split("\r"))
+            data = data.replace(",\n","\n")[:-1]
+        # Write the data stream to disk
+        f = open("%s.csv" % sym, "w")
+        f.write(data)
+        f.close()
---
+++ src/tests/test_import_iqfeed.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 10:19:10 2017
+
+@author: pashute
+"""
+'''
+  see youtube: python unitests https://www.youtube.com/watch?v=6tNS--WetLI
+  testcase documentation: https://docs.python.org/2/library/unittest.html#unittest.TestCase
+'''
+import unittest
+
+class TestIqFeedImport(unittest.TestCase):
+    def test_import_iqfeed(self):
+        result = false  // failing by design
+        assertTrue(result)
+        
+    def test_import_feeds(self):
+        result = false // failing by design
+        assertTrue(result)
