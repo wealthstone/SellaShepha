fcbc945508caaf735b131a14c5bbbcd2f9966aff
HofniGartner <gartnerhof@gmail.com>
2017-10-26 18:56:04 +0300

    unittest and spyder project
    running not checked yet, but corrected

--- IQFeeder/tests/testImportIQFeed.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 24 10:19:10 2017
-
-@author: pashute
-"""
-
--- src/tests/test_import_iqfeed.py
+++ src/tests/test_import_iqfeed.py
@@ -7,6 +7,7 @@ Created on Tue Oct 24 10:19:10 2017
 '''
   see youtube: python unitests https://www.youtube.com/watch?v=6tNS--WetLI
   testcase documentation: https://docs.python.org/2/library/unittest.html#unittest.TestCase
+  python -m unittest test_...
 '''
 import unittest
 
@@ -18,3 +19,6 @@ class TestIqFeedImport(unittest.TestCase):
     def test_import_feeds(self):
         result = false // failing by design
         assertTrue(result)
+        
+if __name__ == '__main__':
+    unittest.main()
