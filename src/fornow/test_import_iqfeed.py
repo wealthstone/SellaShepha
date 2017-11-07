620c34be3951b5514c53903aca33c116c6c12a1f
pashute <pashute@gmail.com>
2017-11-07 11:03:00 +0200

    mock iqfeed

--- src/fornow/test_import_iqfeed.py
+++ src/fornow/test_import_iqfeed.py
@@ -14,7 +14,7 @@ Created on Tue Oct 24 10:19:10 2017
 '''
 
 import unittest
-import unittest.mock
+import unittest.mock as mck
 import datetime as dt
 import pandas as pd
 # import numpy as np
@@ -41,29 +41,38 @@ class TestIqFeedImport(unittest.TestCase):
     --'''
 
     def test_import_single_asset__returns_dataframe(self):
-        expected = "tbd: mock"
-        actual = "test not implemented"
-        self.assertEqual(expected, actual)
+        ''' tests that import returns a dataframe using mock '''
+
+        sys.modules['iqfeed'] = mck.Mock()
+        iqf = iqfi.IQFeedImporter()
+
+        symbol = "SPX.XO"
+        date_start = dt.datetime(2015, 10, 1)
+        date_end = dt.datetime(2016, 10, 1)
+
+        actual = iqf.imp1_call_iqfeed(symbol, date_start, date_end)
+        # expected = True
+        self.assertTrue(actual)
 
     def test_import_single_asset__result_validated(self):
+        ''' tests that result is validated'''
         symbol = "asymbol"  # todo: test settings?
         dframe = __fakeimported(symbol)                    
 
-        iqimporter = iqfi.IQFeedImporter()
-        actual = iqimporter.imp1_check_iqfeed_result(dframe)
+        iqf = iqfi.IQFeedImporter()
+        actual = iqf.imp1_check_iqfeed_result(dframe)
         self.assertTrue(actual)
 
     def test_import_single_asset__result_manipulated(self):
+        ''' tests that columns and index inserted in asset dataframe'''
         actual = False  # todo: call method with dataframe and symbol
         self.assertTrue(actual)
 
     def test_import_single_asset(self):
-        '''
-        Tests all single asset importing
-        '''
+        ''' Tests all single asset importing '''
         symbol = "SPX.XO"
         
-        sys.modules['iqfeed'] = Mock()
+        sys.modules['iqfeed'] = mck.Mock()
         iqf = iqfi.IQFeedImporter()
         # self.assertIsNotNone(iqf, "Cannot create IQFeed class")
 
